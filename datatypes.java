75177e939a2df122384919af5e81f7c0bebbd665
Suryasenthil2001 <suryasenthil2805@gmail.com>
2025-04-03 15:53:12 +0530

    Day-02

---
+++ datatypes.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class datatypes {
+	public static void main(String[]args) {
+		byte age = 10;
+		short s = -32768;
+		int num = 3000000;
+		long phoneno = 9753475687l;
+		float f = 12.73f;
+		double d = 12.2272d;
+		boolean t = true;
+		char c = 'n';
+		System.out.println(age);
+		System.out.println(s);
+		System.out.println(num);
+		System.out.println(phoneno);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		
+	}
+
+}
